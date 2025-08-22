d549ada3dfce46a5f0bac25b9a7f201d3b8fd4d6
Fujita2148 <pxh01622@nifty.com>
2025-08-22 21:55:33 +0900

    refactor: split persona editor into modules

---
+++ tools/editor/fileHandler.js
@@ -0,0 +1,46 @@
+export default class FileHandler {
+    constructor(personaData, uiController) {
+        this.personaData = personaData;
+        this.uiController = uiController;
+    }
+
+    async loadFile() {
+        return new Promise((resolve) => {
+            const input = document.getElementById('file-input');
+            input.onchange = (e) => {
+                const file = e.target.files[0];
+                if (file) {
+                    const reader = new FileReader();
+                    reader.onload = (e) => {
+                        const content = e.target.result;
+                        if (this.personaData.fromYAML(content)) {
+                            this.uiController.showNotification('ファイルを正常に読み込みました', 'success');
+                            resolve(true);
+                        } else {
+                            this.uiController.showNotification('ファイルの読み込みに失敗しました', 'error');
+                            resolve(false);
+                        }
+                    };
+                    reader.readAsText(file);
+                }
+            };
+            input.click();
+        });
+    }
+
+    saveFile(filename = 'persona.yaml') {
+        const yamlContent = this.personaData.toYAML();
+        const blob = new Blob([yamlContent], { type: 'text/yaml' });
+        const url = window.URL.createObjectURL(blob);
+        
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+        
+        this.uiController.showNotification('ファイルを保存しました', 'success');
+    }
+}
